ea919289a0c023e2c95bd71497320d6b4803f403
Nitish Mehta <nmehta91@ucla.edu>
2016-02-04 01:53:56 +0000

    Challenge Completed

---
+++ app/API/timestamp_micro.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var moment = require('moment');
+
+module.exports = function(app) {
+    app.get('/:query', function(request, response) {
+        var date = request.params.query;
+        var unix = null;
+        var natural = null;
+        
+        if(typeof date === "number"){
+            unix = date;
+            natural = unixToNatural(unix);
+        }
+        
+        else if(isNaN(date) && moment(date, "MMMM D, YYYY").isValid()){
+            unix = naturalToUnix(date);
+            natural = date;
+        }
+        
+        var dateJSON = {
+          unix: unix,
+          natural: natural
+        };
+       
+       response.send(JSON.stringify(dateJSON));
+    });
+    
+    function naturalToUnix(ndate){
+        return moment(ndate, "MMMM D, YYYY").format("X");
+    }
+    
+    function unixToNatural(xdate){
+        return moment.unix(xdate).format("MMMM D, YYYY");
+    }
+}
